0b9e1bd2aca0b7a997321d76ed5f2df55039ba66
myrepo/src/com/j2 <kelle111@naver.com>
2017-11-28 01:56:35 -0500

    added server

--- maze.py
+++ maze.py
@@ -1,11 +1,74 @@
 # star-trek-project-class-saber
 CSCI 121
+# add light
+# add server
 
 from Myro import*
+from Myro import*
+import random
+import urllib2
 
+
+def postImage(url,fileName):
+    body= '';
+    try:
+        file = open(fileName,'rb')
+        body = file.read()
+        file.close()
+    except OSError:
+        return b''
+
+    part_boundary = b'---------------------Boundary'+bytes(str(int(random.random()*1e10)),'utf-8')
+    data = []
+    data.append(b'--'+part_boundary)
+    data.append(bytes('Content-Disposition: form-data; name="file"; filename="{}"'.format(fileName),'utf-8'))
+    data.append(bytes('Content-Type: {}'.format('image/png'),'utf-8'))
+    data.append(b'')
+    data.append(body)
+    data.append(b'--' + part_boundary + b'--')
+    data.append(b'')
+    body = b''
+    for item in data:
+        body += item + b'\r\n'
+
+    request = urllib2.Request(url)
+    # adding charset parameter to the Content-Type header.
+    request.add_header("Content-Type","multipart/form-data;boundary={}".format(str(part_boundary)))
+    request.add_header('Content-length', len(body))
+    webHandle = urllib2.urlopen(request,body)
+    reply = webHandle.read();
+    webHandle.close()
+    return reply
+
+def deleteCommand(url):
+    request = urllib2.Request(url)
+    request.get_method = lambda: 'DELETE'
+    webHandle = urllib2.urlopen(request)
+    command = webHandle.read();
+    webHandle.close()
+    return command
+
+def getCommand(url):
+    request = urllib2.Request(url)
+    webHandle = urllib2.urlopen(request)
+    command = webHandle.read();
+    webHandle.close()
+    return command
+
+def sendPicToServer(picName):
+    url ='http://localhost:8080/robot'
+    postImage(url,picName)
+    cmdInJSONFormat = getCommand(url) # example command get
+    #cmdOutJSONFormat = deleteCommand(url) # example command delete
+    print(cmdInJSONFormat)
+
+           
 def move():
     
     currentLight=getLight()
+    num=1
+    picName='photo#%s.jpg'%format(num)
+    url ='http://localhost:8080/robot'
 
     while True:
         getLine()
@@ -17,16 +80,12 @@ def move():
             turnRight(1,1.2)
             pic=takePicture()
             show(pic)
-            #save(pic)
-            turnRight(1,.2)
-            pic=takePicture()
-            show(pic)
-            #save(pic)
-            turnRight(1,.2)
-            pic=takePicture()
-            show(pic)
-            #save(pic)
-            turnRight(1,1.1)
+            savePicture(pic,picName)
+            sendPicToServer(picName)
+            num+=1
+            picName='photo#%s.jpg'%format(num)
+           
+            turnRight(1,1.5)
             elapsedLight=getLight()
         
         #Just in case, for whatever reason, the robot is still on the line
